580854306e094afa85414711a8d1127a71f9dd4c
hbelfer <hbelfer@gmail.com>
2023-05-29 15:53:47 +0300

    fix link appears twice

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -40,19 +40,3 @@ CREATE TABLE `vote` (
   FOREIGN KEY (`link_id`) REFERENCES `link` (`id`)
 );
 
-
---
--- Table structure for table `link`
---
-
-DROP TABLE IF EXISTS `link`;
-CREATE TABLE `link` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `created_by` varchar(255) DEFAULT NULL,
-  `creation_date` datetime DEFAULT NULL,
-  `last_modified_by` varchar(255) DEFAULT NULL,
-  `last_modified_date` datetime DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ;
